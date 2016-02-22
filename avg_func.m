8f299ace839ab772d85663a68370361c21b4f972
mrinaljoshi <mrinal1308@gmail.com>
2016-02-22 09:34:53 -0800

    Update avg_func.m

--- avg_func.m
+++ avg_func.m
@@ -1,46 +1,46 @@
 
 function [avg_fvec1 , avg_fvec2 , avg_fvec3 ,avg_fvec4, avg_fvec5] = avg_func()
-path = 'C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures';
-A1 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\A1.jpg')),0.8)));
-A2 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\A2.jpg')),0.8)));
-A3 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\A3.jpg')),0.8)));
-A4 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\A4.jpg')),0.8)));
-A5 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\A5.jpg')),0.8)));
+
+A1 = double(1 -(im2bw(rgb2gray(imread('..path\A1.jpg')),0.8)));
+A2 = double(1 -(im2bw(rgb2gray(imread('..path\A2.jpg')),0.8)));
+A3 = double(1 -(im2bw(rgb2gray(imread('..path\A3.jpg')),0.8)));
+A4 = double(1 -(im2bw(rgb2gray(imread('..path\A4.jpg')),0.8)));
+A5 = double(1 -(im2bw(rgb2gray(imread('..path\A5.jpg')),0.8)));
 
 
 avg_fvec1 = avg_fvec(A1,A2,A3,A4,A5);
 
-A1 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\B1.jpg')),0.8)));
-A2 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\B2.jpg')),0.8)));
-A3 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\B3.jpg')),0.8)));
-A4 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\B4.jpg')),0.8)));
-A5 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\B5.jpg')),0.8)));
+A1 = double(1 -(im2bw(rgb2gray(imread('..path\B1.jpg')),0.8)));
+A2 = double(1 -(im2bw(rgb2gray(imread('..path\B2.jpg')),0.8)));
+A3 = double(1 -(im2bw(rgb2gray(imread('..path\B3.jpg')),0.8)));
+A4 = double(1 -(im2bw(rgb2gray(imread('..path\B4.jpg')),0.8)));
+A5 = double(1 -(im2bw(rgb2gray(imread('..path\B5.jpg')),0.8)));
 
 
 avg_fvec2  = avg_fvec(A1,A2,A3,A4,A5);
 
-A1 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\C1.jpg')),0.8)));
-A2 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\C2.jpg')),0.8)));
-A3 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\C3.jpg')),0.8)));
-A4 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\C4.jpg')),0.8)));
-A5 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\C5.jpg')),0.8)));
+A1 = double(1 -(im2bw(rgb2gray(imread('..path\C1.jpg')),0.8)));
+A2 = double(1 -(im2bw(rgb2gray(imread('..path\C2.jpg')),0.8)));
+A3 = double(1 -(im2bw(rgb2gray(imread('..path\C3.jpg')),0.8)));
+A4 = double(1 -(im2bw(rgb2gray(imread('..path\C4.jpg')),0.8)));
+A5 = double(1 -(im2bw(rgb2gray(imread('..path\C5.jpg')),0.8)));
 
 
 avg_fvec3  = avg_fvec(A1,A2,A3,A4,A5);
 
-A1 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\D1.jpg')),0.8)));
-A2 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\D2.jpg')),0.8)));
-A3 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\D3.jpg')),0.8)));
-A4 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\D4.jpg')),0.8)));
-A5 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\D5.jpg')),0.8)));
+A1 = double(1 -(im2bw(rgb2gray(imread('..path\D1.jpg')),0.8)));
+A2 = double(1 -(im2bw(rgb2gray(imread('..path\D2.jpg')),0.8)));
+A3 = double(1 -(im2bw(rgb2gray(imread('..path\D3.jpg')),0.8)));
+A4 = double(1 -(im2bw(rgb2gray(imread('..path\D4.jpg')),0.8)));
+A5 = double(1 -(im2bw(rgb2gray(imread('..path\D5.jpg')),0.8)));
 
 avg_fvec4  = avg_fvec(A1,A2,A3,A4,A5);
 
-A1 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\E1.jpg')),0.8)));
-A2 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\E2.jpg')),0.8)));
-A3 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\E3.jpg')),0.8)));
-A4 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\E4.jpg')),0.8)));
-A5 = double(1 -(im2bw(rgb2gray(imread('C:\Users\Mrinal\Downloads\Old asgns\Old asgns\Wavlet\Project\Pictures\E5.jpg')),0.8)));
+A1 = double(1 -(im2bw(rgb2gray(imread('..path\E1.jpg')),0.8)));
+A2 = double(1 -(im2bw(rgb2gray(imread('..path\E2.jpg')),0.8)));
+A3 = double(1 -(im2bw(rgb2gray(imread('..path\E3.jpg')),0.8)));
+A4 = double(1 -(im2bw(rgb2gray(imread('..path\E4.jpg')),0.8)));
+A5 = double(1 -(im2bw(rgb2gray(imread('..path\E5.jpg')),0.8)));
 
 avg_fvec5  = avg_fvec(A1,A2,A3,A4,A5);
 end
